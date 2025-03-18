bdf49e85f4b06604019985aee7e3eb1fe50f0b94
teorebull <122378752+teorebull@users.noreply.github.com>
2025-03-19 00:25:01 +0100

    Update seleccio_infraccions.py

--- seleccio_infraccions.py
+++ seleccio_infraccions.py
@@ -5,29 +5,36 @@ def seleccionar_accio():
         accio = input('Escull entre Afegir, Eliminar i Sortir: ')
 
         # Si aquesta opció es selecciona sortirem del programa
-        if accio == 'Sortir':
+        if accio.lower() == 'sortir':
             print('Programa finalitzat')
             return f'Aquesta és la llista després de les modificacions: {llista}'
-            break
-        if accio == 'Afegir':
+            
+        if accio.lower() == 'afegir':
             # Mentres l'input de l'usuari sigui un int, li seguirem demanant d'afegir codis
             while True:
                 try:
-                    afegir = int(input("Afegeix un codi d'infracció: "))
-                    if afegir not in llista:
-                        llista.append(afegir)
+                    codi = int(input("Afegeix un codi d'infracció: "))
+                    if codi not in llista:
+                        llista.append(codi)
+                        print(llista)
+                    else:
+                        print('Aquest codi ja hi és a la llista')
                         print(llista)
                 except ValueError:
-                    break                    
-        elif accio == 'Eliminar':
-            # Cada cop que s'elimini un codi, es tornarà al menú principal
-            eliminar = int(input('Elimina una infracció: '))
-            if eliminar not in llista:
-                print('El codi que ha seleccionat no està a la llista.')
-            else:
-                llista.remove(eliminar)
-                print(llista)
-            
-             
+                    break                   
+        elif accio.lower() == 'eliminar':
+            try:
+                # Cada cop que s'elimini un codi, es tornarà al menú principal
+                infraccio = int(input('Elimina una infracció: '))
+                if infraccio not in llista:
+                    print('El codi que ha seleccionat no està a la llista.')
+                else:
+                    llista.remove(infraccio)
+                    print(llista)
+                    
+            except ValueError:
+                    print('Has d\'escriure un codi vàlid')
+                
+                    
                 
 print(seleccionar_accio())
